8a7e82169dcbc2e8f822544ead5e065b95c27121
23002su <23002@supnum.mr>
2025-07-09 12:43:49 +0100

    fixed the translation client

--- packages/locales/server.ts
+++ packages/locales/server.ts
@@ -2,6 +2,6 @@
 import { createI18nServer } from "next-international/server";
 
 export const { getI18n, getScopedI18n, getStaticParams } = createI18nServer({
-  fr: () => import("./translations/fr.js"),
-  ar: () => import("./translations/ar.js"),
+  fr: () => import("./translations/fr"),
+  ar: () => import("./translations/ar"),
 });
